4cfbaa40de82fd8a8eec52ebeff226c937cf39ba
Hepheir <hepheir@gmail.com>
2024-03-09 01:38:53 +0900

    TLE: O(n log n + m log m)에 풀이

--- 02주차 구간합/김동주/boj_24838.py
+++ 02주차 구간합/김동주/boj_24838.py
@@ -1,35 +1,32 @@
 from sys import stdin
 from sys import stdout
-from functools import cache
-from functools import reduce
-from typing import Iterable
 
 
 MOD = int(1e9+7)
+MAX_N = 50000
+MOD_FACTORIAL = []
 
 
+"""
+나머지의 분배법칙 이용
+
+    (A * B) mod C = (A mod C * B mod C) mod C
+
+    https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-multiplication
+"""
 def madd(a: int, b: int, mod=MOD) -> int:
     return ((a % mod) + (b % mod)) % mod
 
 
 def mmul(a: int, b: int, mod=MOD) -> int:
-    """나머지가 있는 곱을 구하는 함수.
-    (A * B) mod C = (A mod C * B mod C) mod C
-
-    https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-multiplication
-    """
     return ((a % mod) * (b % mod)) % mod
 
 
-def msum(x: Iterable[int], mod=MOD) -> int:
-    return reduce(madd, x)
-
-
-@cache
-def factorial(n: int) -> int:
-    if n == 0:
-        return 1
-    return mmul(factorial(n-1), n)
+def init_mod_factorial():
+    # O(n)
+    MOD_FACTORIAL.append(1)
+    for i in range(1, MAX_N+1):
+        MOD_FACTORIAL.append(mmul(MOD_FACTORIAL[-1], i))
 
 
 def testcase():
@@ -37,42 +34,44 @@ def testcase():
     # 나머지연산은 덧셈, 곱셈에 대해 분배법칙이 적용됨에 주목할 것.
 
     n, m = map(int, stdin.readline().split())
-    A = list(map(int, stdin.readline().split()))
-    A_cnt = [0] * n # A의 각 원소가 포함된 구간의 개수
 
-    # O(n * m)
+    # O(n log n)
+    A = list(map(int, stdin.readline().split()))
+    A.sort()
+
+    # O(m)
+    events = []
     for j in range(m):
         x, y = map(int, stdin.readline().split())
-        for i in range(x-1, y): # y-x 는 최대 n
-            A_cnt[i] += 1
+        events.append((x, +1)) # rising edge
+        events.append((y+1, -1)) # falling edge
+    # O(m log m)
+    events.sort()
 
-    # O(n log n) 기본적으로 오름차순 정렬
-    A.sort()
-    A_cnt.sort()
+    # O(m)
+    frequency = [0] * (m + 1)
+    last_index = 1
+    last_n_ranges = 0
+    for index, diff in events:
+        frequency[last_n_ranges] += (index - last_index)
+        last_n_ranges += diff
+        last_index = index
+    frequency[0] += (n + 1 - last_index)
 
+    # O(n+m)
+    # (A는 오름차순으로 정렬되어 있음.)
     max_s = 0
     n_cases = 1
-
-    while A_cnt and A_cnt[-1] != 0:
-        count = A_cnt.pop() # A의 한 원소가 포함된 구간의 개수
-        length = 1 # 같은 count 값을 갖는 구간의 길이
-
-        a = A.pop()
-        while A_cnt and A_cnt[-1] == count:
-            a = madd(a, A.pop())
-            A_cnt.pop()
-            length += 1
-
-        max_s = madd(max_s, mmul(a, count))
-        n_cases = mmul(n_cases, factorial(length))
-
-    # 아무 구간에도 포함하지 않은 원소를 배치하는 경우의 수 계산
-    length = len(A_cnt)
-    n_cases = mmul(n_cases, factorial(length))
+    for n_ranges, n_elements in reversed(list(enumerate(frequency))):
+        # n_ranges개의 구간에 포함된 원소가 n_elements개.
+        for i in range(n_elements):
+            max_s += n_ranges * A.pop()
+        n_cases = mmul(n_cases, MOD_FACTORIAL[n_elements]) # 원소를 배치하는 경우의 수
 
     stdout.write(f'{max_s} {n_cases}\n')
 
 
 if __name__ == "__main__":
+    init_mod_factorial()
     for t in range(int(stdin.readline())):
         testcase()
