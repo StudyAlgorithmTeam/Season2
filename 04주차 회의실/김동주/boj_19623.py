79a1dad3db95aab1e0f8a4cd404a9caebd351eec
Hepheir <hepheir@gmail.com>
2024-03-25 01:36:59 +0900

    AC(576ms): dp + binary search O(n log n)

--- 04주차 회의실/김동주/boj_19623.py
+++ 04주차 회의실/김동주/boj_19623.py
@@ -14,16 +14,15 @@ def testcase():
     meetings = [ tuple(map(int, stdin.readline().split())) for _ in range(N) ]
     meetings.sort(key=lambda m: m[END]) # 종료 시간 기준으로 정렬
 
-    dp = [0] * N # i 번째 회의가 끝났을 때, 최대 인원
+    dp = [0] * (N+1) # i 번째 회의가 끝났을 때, 최대 인원
 
     for i in range(N):
-        dp[i] = meetings[i][COUNT]
+        dp[i] = max(dp[i-1], meetings[i][COUNT])
 
         j = bisect_left(meetings, meetings[i][START], lo=0, hi=i, key=lambda m: m[END])
 
-        for k in range(j):
-            if dp[i] < dp[k] + meetings[i][COUNT]:
-                dp[i] = dp[k] + meetings[i][COUNT]
+        if dp[i] < dp[j-1] + meetings[i][COUNT]:
+            dp[i] = dp[j-1] + meetings[i][COUNT]
 
     stdout.write(str(max(dp))+'\n')
 
