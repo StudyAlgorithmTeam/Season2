a0d85a3babd9f7c578374d2607660386fc375c81
Hepheir <hepheir@gmail.com>
2024-03-25 01:29:48 +0900

    WA: dp 로 풀이 O(N^2)

--- 04주차 회의실/김동주/boj_19623.py
+++ 04주차 회의실/김동주/boj_19623.py
@@ -1,6 +1,11 @@
-from collections import deque
+from bisect import bisect
 from sys import stdin
 from sys import stdout
+
+
+START = 0
+END = 1
+COUNT = 2
 
 
 def testcase():
@@ -9,32 +14,16 @@ def testcase():
     meetings = [ tuple(map(int, stdin.readline().split())) for _ in range(N) ]
     meetings.sort()
 
-    q = deque([(0, 0, None)]) # (회의가 끝난 시간, 누적 인원 수, 파기 시간)
+    dp = [0] * (N+1)
 
-    for s, e, c in meetings:
-        for _ in range(len(q)):
-            q_end, q_cnt, q_exp = q.popleft()
+    for i in range(N):
+        j = bisect(meetings, meetings[i][END], key=lambda m: m[START])
 
-            if q_exp is not None and q_exp < s:
-                continue
+        for k in range(j, N+1):
+            if dp[k] < dp[i] + meetings[i][COUNT]:
+                dp[k] = dp[i] + meetings[i][COUNT]
 
-            # 이번 회의를 진행 할 경우:
-            if q_end <= s:
-                q.append((e, q_cnt+c, None))
-
-            if q_exp is None and q_end < e:
-                q.append((q_end, q_cnt, e))
-            else:
-                q.append((q_end, q_cnt, q_exp))
-        pass
-
-    max_cnt = 0
-    while q:
-        cnt = q.pop()[1]
-        if cnt > max_cnt:
-            max_cnt = cnt
-
-    stdout.write(str(max_cnt)+'\n')
+    stdout.write(str(dp[N])+'\n')
 
 
 if __name__ == '__main__':
