1381822c77efc9b28052f73d138991cfcc519449
UHyeonj <128715793+UHyeonj@users.noreply.github.com>
2024-03-25 12:20:27 +0900

    Update 1788_TLE.py

--- 04주차 회의실/정우현/1788_TLE.py
+++ 04주차 회의실/정우현/1788_TLE.py
@@ -7,7 +7,7 @@ def fibonaci(i):
         x = farr.pop(0) + farr[0]
         farr.append(x)
     return(farr[1]) 
-
+#Big Integer - int longlong을 넘어가는 숫자가 되면 처리를 못한다. 문자열로 만들거나(BCD) 오버플로우가 나는 곳을 기준으로 배열을 나누는 식으로 한다.  
 n = int(input())
 
 
