b0f79735caf4ddb837ee060c92e2d936ac9afe55
Hepheir <hepheir@gmail.com>
2024-04-07 04:05:48 +0900

    TLE: DP, i부터 j번째 가지 행렬을 구할 때의 최소 연산 수

--- 06주차 Indexed Priority Queue/김동주/extra/boj_11049.py
+++ 06주차 Indexed Priority Queue/김동주/extra/boj_11049.py
@@ -1,37 +1,17 @@
 import functools
 import sys
-import typing
 
-
-MAX_N = 500
 
 N = int(sys.stdin.readline())
-
-shape = []
-for i in range(N):
-    r, c = map(int, sys.stdin.readline().split())
-    shape.append((r, c))
+shape = [tuple(map(int, sys.stdin.readline().split())) for i in range(N)]
 
 
 @functools.cache
-def dp(i: int, r: int) -> typing.Tuple[int, int]: # value, c
-    if i == N-1:
-        return 0, shape[i][1]
-
-    ans = []
-
-    # 나를 먼저 곱할 때 = (AB)C
-    ops = r * shape[i][1] * shape[i+1][1]
-    val, c = dp(i+1, r)
-    ans.append((ops+val, min(c, shape[i+1][1])))
-
-    # 나를 나중에 곱할 때 = A(BC)
-    val, c = dp(i+1, shape[i+1][0])
-    ops = r * shape[i][1] * c
-    ans.append((ops+val, c))
-
-    return min(ans)
+def dp(i: int, j: int) -> int:
+    if i+1 == j:
+        return shape[i][0] * shape[i][1] * shape[j][1]
+    return min([dp(i, k) + dp(k+1, j) for k in range(i+1, j)], default=sys.maxsize)
 
 
-sys.setrecursionlimit(MAX_N*4)
-sys.stdout.write(f'{dp(0, shape[0][0])[0]}\n')
+sys.setrecursionlimit(N*4)
+sys.stdout.write(f'{dp(0, N-1)}\n')
