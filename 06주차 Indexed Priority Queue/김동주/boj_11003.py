79d77c86919fe45f517b75ff234df32af371f1e8
Hepheir <hepheir@gmail.com>
2024-04-08 06:14:33 +0900

    AC(5268): Heap, O(N log N)
    (생각해보니 큐를 쓸 필요도 없었던...)

--- 06주차 Indexed Priority Queue/김동주/boj_11003.py
+++ 06주차 Indexed Priority Queue/김동주/boj_11003.py
@@ -1,22 +1,13 @@
-import collections
 import heapq
 from sys import stdin
 from sys import stdout
 
-
-HEAP_ELEM_VALUE = 0
-HEAP_ELEM_KEY = 1
-
 N, L = map(int, stdin.readline().split())
-
 heap = []
-q = collections.deque()
 
+# 원소의 인덱스가 곧 삽입된 시간이라고 하는 것이 메인 컨셉
 for i, A_i in enumerate(map(int, stdin.readline().split())):
-    q.append(A_i)
-    heapq.heappush(heap, (A_i, i))
-    if i >= L:
-        q.popleft()
-        while heap[0][HEAP_ELEM_KEY] < i-L+1:
-            heapq.heappop(heap)
-    stdout.write(str(heap[0][HEAP_ELEM_VALUE])+' ')
+    heapq.heappush(heap, (A_i, i)) # (원소의 값, 삽입된 시간)
+    while heap[0][1] < (i-L+1): # 너무 오래전에 삽입된 것이면 유효하지 않으므로 제거
+        heapq.heappop(heap)
+    stdout.write(str(heap[0][0])+' ')
