4e276a6a94febaae1af143f160d02e25fa8ab82c
Hepheir <hepheir@gmail.com>
2024-04-08 20:51:11 +0900

    AC(88ms): dp, memoize, bitmasking, backtracking, greedy (이거 시간 복잡도는 어떻게 구해야 할지 모르겠음)

--- 07주차 Two-Pointer/김동주/extra/boj_1040.py
+++ 07주차 Two-Pointer/김동주/extra/boj_1040.py
@@ -1,40 +1,66 @@
-import collections
+import functools
 import sys
-import typing
 
 
-
-N: typing.List[int]
+N: int
 K: int
 
-COUNTER = collections.Counter()
+
+@functools.cache
+def dp(n: int, deg: int, flag: int) -> int:
+    """10진수 deg자리 수인 n 보다 크거나 같은 수 중 K개의 숫자로 이루어진 수를 찾음
+
+    단, deg 앞 자리 수에서 flag에 표기된 숫자들은 이미 사용 된 것으로 가정.
+
+    n <= 10^18
+    deg <= 18
+    flag < 2^10
+    """
+    k = get_k(flag)
+    if k > K:
+        return None
+    if deg == 0:
+        return None if k != K else 0
+
+    base = 10 ** (deg-1)
+    msb = n // base
+    rem = n % base # remainder
+
+    while msb < 10:
+        if (ans := dp(rem, deg-1, flag | (1 << msb))) is not None:
+            return (msb * base) + ans
+        msb += 1
+        rem = 0
+
+    return None
 
 
-def dp(i: int, k: int, carry: bool = False) -> bool:
-    if i == -1:
-        return k == 0
-    old_val = N[i]
-    s = 0 if carry else N[i]
-    for n in range(s, 10):
-        if COUNTER[n] == 0:
-            k -= 1
-        COUNTER[n] += 1
+def get_deg(n: int) -> int:
+    """n이 몇 자릿 수인지 반환"""
+    deg = 0
+    while n:
+        deg += 1
+        n //= 10
+    return deg
 
-        N[i] = n
-        if dp(i-1, k, carry):
-            return True
-        carry = True
 
-        COUNTER[n] -= 1
-        if COUNTER[n] == 0:
+def get_k(flag: int) -> int:
+    """플래그를 보고 지금까지 사용된 숫자의 개수를 반환"""
+    k = 0
+    for i in range(10):
+        if (flag & (1 << i)):
             k += 1
-    N[i] = old_val
-    return False
+    return k
 
 
 if __name__ == '__main__':
-    N, K = sys.stdin.readline().split()
-    N = list(map(int, reversed(N)))
-    K = int(K)
-    dp(len(N)-1, K)
-    sys.stdout.write(f'{"".join(map(str, reversed(N)))}\n')
+    N, K = map(int, sys.stdin.readline().split())
+
+    n = N
+    deg = get_deg(n)
+    # 해보고 안 되면 자릿 수 늘려서 다시 해보기
+    while (ans := dp(n, deg, 0)) is None:
+        n = 10 ** deg
+        deg += 1
+
+    sys.stdout.write(f'{ans}\n')
