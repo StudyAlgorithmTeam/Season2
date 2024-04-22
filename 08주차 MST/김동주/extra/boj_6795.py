d6908ba638562de77f0aec622429ca23884eb44c
Hepheir <hepheir@gmail.com>
2024-04-23 07:06:25 +0900

    AC(40ms): [B2] 그냥 수학식으로 품

---
+++ 08주차 MST/김동주/extra/boj_6795.py
@@ -0,0 +1,25 @@
+def calc(fwd, bwd, s):
+    dist = (fwd-bwd) * (s//(fwd+bwd)) # dist so far
+    rem = s % (fwd+bwd) # dist remains
+    if rem <= fwd:
+        dist += rem
+    else:
+        dist -= rem - fwd
+    return dist
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+s = int(input())
+
+nikky = calc(a, b, s)
+byron = calc(c, d, s)
+
+if nikky > byron:
+    print('Nikky')
+elif nikky < byron:
+    print('Byron')
+else:
+    print('Tied')
