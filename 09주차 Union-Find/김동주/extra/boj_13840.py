92a37dcfd86156b42b8529cb1e4eb95d40d553b3
Hepheir <hepheir@gmail.com>
2024-05-02 00:30:53 +0900

    AC(220ms): #mathematics #calculus #sweeping O(n)
    Autocorrelation을 정적분을 이용하여 풀이
    + 동일 풀이, 일부 리펙토링

--- 09주차 Union-Find/김동주/extra/boj_13840.py
+++ 09주차 Union-Find/김동주/extra/boj_13840.py
@@ -1,4 +1,4 @@
-# Autocorrelation
+# Autocorrelation Function
 
 from collections import deque
 import sys
@@ -30,8 +30,7 @@ if __name__ == "__main__":
 
         # 작은 x 좌표부터 꺼내가며 각 때의 y값을 구함.
         x0 = MIN_X + MIN_R - 1 # divide by zero 방지
-        f0 = 0.0
-        g0 = 0.0
+        f0, g0 = 0.0, 0.0
         while f and g:
             # x0 이후로 가장 먼저 나오는 x 좌표 x1을 찾음.
             # 필요하다면, 선형 보간으로 f 혹은 g값을 구함.
@@ -41,35 +40,26 @@ if __name__ == "__main__":
                 x1, f1 = f.popleft()
                 x1, g1 = g.popleft()
                 a = (f1-f0)/(x1-x0)
-                b = f0 - a*x0
                 c = (g1-g0)/(x1-x0)
-                d = g0 - c*x0
             elif f[0][0] < g[0][0]:
                 x1, f1 = f.popleft()
                 a = (f1-f0)/(x1-x0)
-                b = f0 - a*x0
                 c = (g[0][1]-g0)/(g[0][0]-x0)
-                d = g0 - c*x0
-                g1 = c*x1 + d
             else:
                 x1, g1 = g.popleft()
                 a = (f[0][1]-f0)/(f[0][0]-x0)
-                b = f0 - a*x0
                 c = (g1-g0)/(x1-x0)
-                d = g0 - c*x0
-                f1 = a*x1 + b
+            b = f0 - a*x0
+            d = g0 - c*x0
+            f1 = a*x1 + b
+            g1 = c*x1 + d
 
             # Online integral calculator로 구한 공식 적용
             # antiderivative of (ax+b)(cx+d) dx is...
             # ... ac(x^3)/3 + (ad+bc)(x^2)/2 + bdx
             # 정적분이므로 양단의 값의 차로 구간 적분 값을 구함 (적분상수는 사라짐)
-            Y0 = a*c*(x0**3)/3 + (a*d + b*c)*(x0**2)/2 + b*d*x0
-            Y1 = a*c*(x1**3)/3 + (a*d + b*c)*(x1**2)/2 + b*d*x1
+            autocorrelation += a*c*(x1**3-x0**3)/3 + (a*d+b*c)*(x1**2-x0**2)/2 + b*d*(x1-x0)
 
-            autocorrelation += Y1-Y0
-
-            x0 = x1
-            f0 = f1
-            g0 = g1
+            x0, f0, g0 = x1, f1, g1
 
         sys.stdout.write(f"{autocorrelation}\n")
