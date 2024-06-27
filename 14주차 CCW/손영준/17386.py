8f06cbba59fdd0495f2b744976d98591ec4872e8
SonYoungJun <63299624+young-dune@users.noreply.github.com>
2024-06-27 12:59:38 +0900

    Create 17386.py

---
+++ 14주차 CCW/손영준/17386.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+
+def ccw(x1, y1, x2, y2, x3, y3):
+    return  x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3
+
+
+res1 = ccw(x1, y1, x2, y2, x3, y3)
+res2 = ccw(x1, y1, x2, y2, x4, y4)
+res3 = ccw(x3, y3, x4, y4, x1, y1)
+res4 = ccw(x3, y3, x4, y4, x2, y2)
+
+if res1 * res2 < 0 and res3 * res4 < 0:
+    print(1)
+else :
+    print(0)
+    
